7e57df856199e8c7afc26360a9f6d85a6d5f0ce7
Ruben <rubencover2006@gmail.com>
2021-10-05 00:42:57 -0600

    Added eureka client to micro services for registry

--- inventory-service/src/main/java/com/example/DemoApplication.java
+++ inventory-service/src/main/java/com/example/DemoApplication.java
@@ -2,14 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.repository")
-@EntityScan(basePackages = "com.example.domain")
-@ComponentScan(basePackages = "com.example")
+@EnableEurekaClient
 public class DemoApplication {
 
     public static void main(String[] args) {
